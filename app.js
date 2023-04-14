6e3b44d84a2dc4789a6f4ae6d8158398d3828609
Antonio Caceres <antonio.caceres178@gmail.com>
2023-04-14 11:19:36 -0400

    no access key

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors'
 import HelloController from './controllers/hello-controller.js';
 import UserController from "./controllers/users/users-controller.js";
-import TuitsController from './db/tuits/tuits-controller.js';
+import TuitsController from './controllers/tuits/tuits-controller.js';
 import mongoose from "mongoose";
 const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING  || "mongodb+srv://admin:Gambino2002@tuiter.18jkx14.mongodb.net/?retryWrites=true&w=majority";
+  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/tuits";
   mongoose.connect(CONNECTION_STRING);
 
 const app = express();
